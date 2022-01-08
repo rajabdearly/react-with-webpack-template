647194cc1edc7132e036f942076935cfed294024
Rajab Azizli <rajabdearly@gmail.com>
2022-01-09 01:59:03 +0200

    added on btn click event

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
-
 import React from 'react';
 import styles from './styles.scss';
+
+
+const onClickEvent = (e) => {
+    e.preventDefault();
+    alert('You Clicked Me!')
+}
+
 const App = () => {
     return (
         <div className={styles.content}>
             <div className={styles.label}>
                 Create React App Without CRA😊
             </div>
-            <button className={styles.btn}>Click Me 😎</button>
+            <button className={styles.btn} onClick={onClickEvent}>Click Me 😎</button>
         </div>
     )
 }
